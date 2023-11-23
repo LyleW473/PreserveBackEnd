dc6af9215a2e79eea1430f1a93c3d284dca56ad9
Lyle Wong <wlyle473@gmail.com>
2023-11-23 01:48:32 +0000

    Adding code completed during CultureHack: - Added backend files used to retrieve / input data into a MongoDB database when signing up/ logging in. - Added progress on back-end code for creating a listing. - Deployed a lambda function for code inference (for authentication so far).

---
+++ foodListing/index.mjs
@@ -0,0 +1,70 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+export const handler = async (event) => {
+    const payload = JSON.parse(event.body);
+
+    try
+        {
+            require("dotenv").config();
+            const uri = process.env.MONGO_DB_URI;
+
+            // Create and connect to MongoClient (With MongoClientOptions object for stable API version)
+            const client = new MongoClient(uri, {
+                serverApi: {
+                    version: ServerApiVersion.v1,
+                    strict: true,
+                    deprecationErrors: true,
+                }
+            });
+            await client.connect()
+            const database = client.db("PreserveBackendDB")
+            const listingsCollection = database.collection(("listingsCollection"))
+
+            // Payload for creating listings
+            // Image URL
+            // Location
+            // Description
+            // Expiration date
+            // UserID
+            // PostID
+            
+            // Create new listings
+            await listingsCollection.insertOne({...payload})
+
+
+            // Sign up
+            if (payload.university)
+            {   
+                // Create student record for this user
+                await studentCollection.insertOne({...payload})
+
+                // Redirect to main page
+                return {statusCode:307}
+            }
+            
+            // Log in
+            else
+            {   
+                // Attempt to find the login details inside the studentCollection
+                const studentRecord = await studentCollection.findOne({oauthCode: payload.oauthCode})
+
+                // Cannot find the student record
+                if(!studentRecord)
+                {
+                    // Redirect to sign up page
+                    return {statusCode:404}
+                }   
+                else
+                {   
+                    // Unique ID for every student record (automatic)
+                    const userID = studentRecord._id; 
+                    return {statusCode:200, userID} // Redirect to main website + return user ID to show listings
+                }
+            }
+        }
+    
+    catch(exception) 
+        {
+            return {status:500, error:exception.message}
+        }
+};
